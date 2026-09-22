06c571840b526445428aeffa8f981af5b8fb3a89
MrunalNaik <naikmrunal2005@gmail.com>
2026-09-22 12:06:42 +0530

    Configure plugin as panel

--- plugin.js
+++ plugin.js
@@ -2,19 +2,25 @@
 
     window.Asc.plugin.init = function () {
         console.log("Contractzy plugin initialized");
+
+        const button = document.getElementById("insertBtn");
+
+        if (button) {
+            button.addEventListener("click", function () {
+
+                console.log("Insert button clicked");
+
+                window.Asc.plugin.executeMethod(
+                    "PasteText",
+                    ["Hello from Contractzy Plugin!"]
+                );
+
+            });
+        }
     };
 
     window.Asc.plugin.button = function (id) {
         console.log("Button clicked:", id);
-    };
-
-    window.insertText = function () {
-
-        window.Asc.plugin.executeMethod(
-            "PasteText",
-            ["Hello from Contractzy Plugin!"]
-        );
-
     };
 
 })(window, undefined);
